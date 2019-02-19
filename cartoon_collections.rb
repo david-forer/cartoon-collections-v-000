443d24171957e57c018ab17a320960f3f3718a0a
David Forer <david.j.forer@gmail.com>
2019-02-19 12:21:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ def long_planeteer_calls(array)
   array.any? { |e| e.length > 4}
 end
 
-def find_the_cheese
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
     cheese_types.each do |cheese|
       if array.include?(cheese)
